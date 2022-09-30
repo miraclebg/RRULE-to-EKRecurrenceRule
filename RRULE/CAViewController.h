c926c752443d04f872c61959cdd921f04e269fc0
Martin Kovachev <miracle@nimasystems.com>
2022-09-30 15:06:28 +0300

    Changes

---
+++ RRULE/CAViewController.h
@@ -0,0 +1,13 @@
+//
+//  CAViewController.h
+//  RRULE
+//
+//  Created by Jochen Schöllig on 24.04.13.
+//  Copyright (c) 2013 Jochen Schöllig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CAViewController : UIViewController
+
+@end
